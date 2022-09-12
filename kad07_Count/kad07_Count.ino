a2b204fa90e753c58a1845c8889f8da21577f33e
rinriN <2210368@ecc.ac.jp>
2022-09-12 17:09:11 +0900

    add : counter when flag = 0

---
+++ kad07_Count/kad07_Count.ino
@@ -0,0 +1,18 @@
+const int swPin = 17; //const...初期化した後代入されない 
+int count = 0; //カウンタ変数
+int flag = 0; //フラグ初期値 0:押されていない状態
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(swPin, INPUT); // Switch デジタル入力
+}
+
+void loop() {
+    int b = digitalRead(swPin);
+    if(b == 1 && flag == 0){
+        count ++;
+        flag = 1;
+    }else{
+        flag = 0;
+    }
+}
