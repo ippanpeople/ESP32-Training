16f3d4f55421be4bdad7440b22dc91c508d9b7c0
rinriN <2210368@ecc.ac.jp>
2022-09-12 16:44:43 +0900

    modify : change led on/off function into loop()

--- kad05_SwitchLED/kad05_SwitchLED.ino
+++ kad05_SwitchLED/kad05_SwitchLED.ino
@@ -5,20 +5,14 @@ void setup() {
   pinMode(17, INPUT);
 }
 
-void led(int time) {
-  digitalWrite(16, HIGH);
-  delay(time);
-  digitalWrite(16, LOW);
-  delay(200);
-}
-
 void loop() {
   // put your main code here, to run repeatedly:
   int b = digitalRead(17);
   if(b == 1){
-    Serial.println("押された");
+    digitalWrite(16, HIGH);
+    delay(10);
   }else{
-    Serial.println(" ");
+    digitalWrite(16, LOW);
+    delay(10);
   }
-  delay(10);
 }
