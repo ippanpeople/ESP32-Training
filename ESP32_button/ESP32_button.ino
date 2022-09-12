220e8f4e5e2527a53e5ce126675980ad15e17a9b
rinriN <2210368@ecc.ac.jp>
2022-09-12 16:07:23 +0900

    add : serial port monitor

--- ESP32_button/ESP32_button.ino
+++ ESP32_button/ESP32_button.ino
@@ -1,9 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  pinMode(17, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int b = digitalRead(17);
+  Serial.print(b);
 }
