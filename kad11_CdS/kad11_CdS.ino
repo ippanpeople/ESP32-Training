94360fc401161fb8c18d9ed3c660136eded56aa4
rinriN <2210368@ecc.ac.jp>
2022-09-15 14:52:58 +0900

    create : kad12

--- kad11_CdS/kad11_CdS.ino
+++ kad11_CdS/kad11_CdS.ino
@@ -7,6 +7,7 @@ void setup(){
 void loop(){
     int a = analogRead(analogPin);
     float b = a * 3.3 / 4095;
+
     Serial.printf("%3d=%3.2f[V]\n", a, b);
-    delay(10);
+    delay(100);
 }
---
+++ kad12_CdS2LED/kad12_CdS2LED.ino
@@ -0,0 +1,35 @@
+const int analogPin = A17;
+const int pwmPin1 = 4; //GPIO IO4ping
+const int pwmPin2 = 16; //GPIO IO16ping
+
+
+void setup(){
+    Serial.begin(115200);
+    ledcSetup(0, 12899, 8);
+    ledcSetup(1, 12899, 8);
+    ledcAttachPin(pwmPin1, 0);
+    ledcAttachPin(pwmPin2, 1);
+
+}
+
+void loop(){
+    int a = analogRead(analogPin);
+    float b = a * 3.3 / 4095;
+    int darkLevel = 1000;
+    int blue = map(a, 500, 3000, 255, 0);
+    int red = map(a, 500, 3000, 255, 0);
+    
+    if(blue < 0){
+        blue = 0;
+        red = 0;
+    }else if(blue > 255){
+        blue = 255;
+        red = 255;
+    }
+    ledcWrite(0, blue);
+    ledcWrite(1, red);
+
+
+    Serial.printf("%3d=%3.2f[V][blue : %3d][red : %3d]\n", a, b, blue, red);
+    delay(100);
+}
