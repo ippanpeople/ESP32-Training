26dcf1d356b6c9fb7d575df35563aa8a60cb0d0a
rinriN <2210368@ecc.ac.jp>
2022-09-15 13:46:41 +0900

    modify : printf(%3.2f) to print float

---
+++ kad11_CdS/kad11_CdS.ino
@@ -0,0 +1,12 @@
+const int analogPin = A17;
+
+void setup(){
+    Serial.begin(115200);
+}
+
+void loop(){
+    int a = analogRead(analogPin);
+    float b = a * 3.3 / 4095;
+    Serial.printf("%3d=%3.2f[V]\n", a, b);
+    delay(10);
+}
