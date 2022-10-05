8106d2de94eef0e8dc345056113482af9c56f3ec
rinriN <2210368@ecc.ac.jp>
2022-10-05 15:46:09 +0900

    create : kad16 kad17 kad18 kad19

---
+++ kad16_noteChime/kad16_noteChime.ino
@@ -0,0 +1,36 @@
+#define LEDC_CHANNEL_0      0//LEDCのPWMチャネル0から15
+#define LEDC_TIMER_13_BIT   13//LEDCタイマーの精度13ビット
+#define LEDC_BASE_FREQ      5000//LEDCのベース周波数5000Hz
+
+const int buzPin = 23;//圧電ブザーのビン番号GP10 1023pin
+const int NOTE_NONE = NOTE_MAX;//休符
+//各音符の音程（音の高さ）
+int melody[] = {
+  NOTE_E, NOTE_C, NOTE_D, NOTE_G, NOTE_NONE, NOTE_G, NOTE_D, NOTE_E, NOTE_C,NOTE_NONE
+};
+
+//各音符の音階（オクターブ）
+int noteOctaves[] = { 4, 4, 4, 3, 0, 3, 4, 4, 4, 0 };
+//各音符の長さ
+int noteDurations[] = { 2, 2, 2, 1, 4, 2, 2, 2, 1, 4 };
+
+void setup(){
+  Serial.begin(115200);
+  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+  ledcAttachPin(buzPin, LEDC_CHANNEL_0);
+  pinMode(17,INPUT);//点击
+}
+
+void loop(){
+  int b = digitalRead(17);
+  if (b == 1){
+    Serial.println(b);
+    for (int thisNote = 0; thisNote < 10; thisNote++){
+      ledcWriteNote(LEDC_CHANNEL_0, (note_t)melody[thisNote], noteOctaves[thisNote]);
+      int pauseBetweenNotes = 1000 / noteDurations[thisNote] * 1.30;
+      delay (pauseBetweenNotes);
+      ledcWriteTone(LEDC_CHANNEL_0,0);//演奏を止める。
+    }
+  }
+  delay(10);
+}
---
+++ kad19_function2/kad19_function2.ino
@@ -0,0 +1,71 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN   5
+#define LED_PIN2   25
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 5
+#define LED_COUNT2 5
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2(LED_COUNT2, LED_PIN2, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+
+// setup() function -- runs once at startup --------------------------------
+
+void setup() {
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  strip.begin();
+  strip2.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip2.show();
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip2.setBrightness(50);
+}
+
+void loop() {
+//colorWipe(strip.Color(255, 0, 0), 50); // Red
+    colorRun(strip.Color(255, 0, 0), 50);
+    // colorRun(strip2.Color(255, 0, 0), 50);
+    delay(1000);
+    colorRun(strip.Color(0, 255, 0), 50);
+    // colorRun(strip2.Color(0, 255, 0), 50);
+    delay(1000);
+}
+/**
+* colorRun
+*/
+void colorRun(uint32_t color, int wait) {
+    // ここに color 色の LED が 0 番目から wait ミリ秒おきに点滅する処理を入れる 
+    for(int i = 0; i < 5; i++){
+        strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+        strip2.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+        strip.show();
+        strip2.show();
+        delay(wait);
+        strip.clear();
+        strip2.clear();
+        strip.show();
+        strip2.show();
+    }
+}
