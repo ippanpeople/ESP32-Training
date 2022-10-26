fc805e5c5cbcc25d6252e733449f73f146489f70
rinriN <2210368@ecc.ac.jp>
2022-10-26 15:13:18 +0900

    modify : some little miss

--- kad12_CdS2LED/kad12_CdS2LED.ino
+++ kad12_CdS2LED/kad12_CdS2LED.ino
@@ -1,35 +1,26 @@
-const int analogPin = A17;
 const int pwmPin1 = 4; //GPIO IO4ping
 const int pwmPin2 = 16; //GPIO IO16ping
+const int analogPin = A17;
+int darkLevel = 1000; //暗さのしきい値
 
-
-void setup(){
-    Serial.begin(115200);
-    ledcSetup(0, 12899, 8);
-    ledcSetup(1, 12899, 8);
-    ledcAttachPin(pwmPin1, 0);
-    ledcAttachPin(pwmPin2, 1);
-
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(0, 12899, 8);
+  ledcSetup(1, 12899, 8);
+  ledcAttachPin(pwmPin1, 0);
+  ledcAttachPin(pwmPin2, 1);
 }
 
-void loop(){
-    int a = analogRead(analogPin);
-    float b = a * 3.3 / 4095;
-    int darkLevel = 1000;
-    int blue = map(a, 500, 3000, 255, 0);
-    int red = map(a, 500, 3000, 255, 0);
-    
-    if(blue < 0){
-        blue = 0;
-        red = 0;
-    }else if(blue > 255){
-        blue = 255;
-        red = 255;
-    }
-    ledcWrite(0, blue);
-    ledcWrite(1, red);
-
-
-    Serial.printf("%3d=%3.2f[V][blue : %3d][red : %3d]\n", a, b, blue, red);
-    delay(100);
+void loop() {
+  int a = analogRead(analogPin);
+  if(a < darkLevel){
+    ledcWrite(0, 255);
+    ledcWrite(1, 255);
+  }else{
+    ledcWrite(0, 0);
+    ledcWrite(1, 0);
+  }
+  Serial.println(a);
+  delay(10);
 }
+
